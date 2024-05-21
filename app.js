e49cf3b40cf10ef7a9552f6b486430a08b77252b
ArtArti <103097814+ArtArti@users.noreply.github.com>
2024-05-21 17:13:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use(
     credentials: true,
   })
 )
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://blog-wave.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Use built-in and third-party middleware
 app.use(express.urlencoded({ extended: true }));
