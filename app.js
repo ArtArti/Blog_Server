1ffdb4426278c9376c9d442d6804264c39bb0974
Arti <singharti7648@gmail.com>
2024-05-23 12:14:20 +0530

    url update

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ dbconnect();
 // Apply CORS middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"], 
+    origin: ["https://blog-wave.vercel.app"], 
     methods: ['GET', 'POST','PUT','DELETE'], 
     credentials: true,
   })
 )
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://blog-wave.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
